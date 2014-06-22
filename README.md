ae8a58e3d521c2e633ae3862ecbdb4fc9307f6f0
Erik Sjöströmm <metallicgeek@gmail.com>
2014-06-22 13:50:43 +0200

    Download via username and meta tags

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 #Bandcamp Downloader
-
+A simple scripts that downloads albums and tracks from bandcamp, tested on python3.4
+It also adds some meta tags to mp3 so it fits into your library nicely
 ##How to use
 * Run the script from the command line
 * Enter the url of the album or track you wish to download
+* Or you could enter a bandcamp username, and the script will try to download everything the artist has made available 
 
 ##ToDO
+* Add the album artwork
+* Figure out how to figure out the genre
+* <del>Add option to enter username instead of url</del>
+* <del>Fix so it can download individual songs</del>
 * <del>Progressbar</del>
-* Add option to enter username instead of url
-* Fix so the files are ordered according to the album and not in alphabetical order
-* Python 2.7 compatibility 
+* <del>Fix so the files are ordered according to the album and not in alphabetical order</del>
--- main.py
+++ main.py
@@ -6,11 +6,94 @@ import sys
 import re
 import os
 
-def main(url):
-	def download(download_url, title, album, artist):
-		file_name = "{}/{}.mp3".format(album, title)
+def main(reply):
+	def f7(seq):
+		seen = set()
+		seen_add = seen.add
+		return [ x for x in seq if x not in seen and not seen_add(x)]
+
+	def parse_username(user):
+		url = "http://" + user + ".bandcamp.com"
+		try:
+			sock = urlretrieve(url)
+		except Exception as e:
+			print("That's not a valid username, perhaps try with a full url instead")
+			sys.exit()
+		ch = open(sock[0]).read()
+		RE = re.compile('<span class="title">(.*?)</span>')
+		user = RE.findall(ch)[0]
+		RE = re.compile('/album/(.*?)">')
+		albums = RE.findall(ch)
+		for album in albums:
+			print("Downloading Album: " + album)
+			print("="*42)
+			album_url = url + "/album/" + album
+			sock = urlretrieve(album_url)
+			ch = open(sock[0]).read()
+			RE = re.compile('album_title : "(.*?)",')
+			album_name = RE.findall(ch)
+			RE = re.compile('artist : "(.*?)",')
+			artist = RE.findall(ch)
+			RE = re.compile("trackinfo : \[(.*?)\]", re.M)
+			matches = RE.findall(ch)
+			RE = re.compile('"title":"(.*?)","')
+			titles = RE.findall(ch)
+			print(titles)
+			titles = f7(titles)  # Remove stray duplicates, if any
+			print(titles)
+			RE = re.compile('"file":{"mp3-128":"(.*?)"}')
+			songs = RE.findall(ch)
+
+			directory = os.path.join(user + "/" + album_name[0])
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+			for i in range(len(songs)):
+				title = titles[i].replace('"','')
+				track = str(i + 1)
+				download(songs[i], title, album_name[0], artist[0], track, user)
+
+	def simple_parse(url):
+		if not url[:6] == "http://":  # Failsafe
+			url = "http://" + url
+		try:
+			sock = urlretrieve(url)
+		except Exception as e:
+			print("That's not a valid url")
+			sys.exit()
+		ch = open(sock[0]).read()
+		RE = re.compile('album_title : "(.*?)",')
+		album_name = RE.findall(ch)
+		RE = re.compile('artist : "(.*?)",')
+		artist = RE.findall(ch)
+		RE = re.compile("trackinfo : \[(.*?)\]", re.M)
+		matches = RE.findall(ch)
+		RE = re.compile('"title":"(.*?)","')
+		titles = RE.findall(ch)
+		print(titles)
+		titles = f7(titles)  # Remove any stray duplicates, if any
+		print(titles)
+		RE = re.compile('"file":{"mp3-128":"(.*?)"}')
+		songs = RE.findall(ch)
+
+		directory = os.path.join(album[0])
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		for i in range(len(songs)):
+			title = titles[i].replace('"','')
+			track = str(i + 1)
+			download(songs[i], title, album_name[0], artist[0], track)
+
+	def download(download_url, title, album, artist, track, user=False):
+		if user:
+			file_name = "{}/{}/{}.mp3".format(user, album, title)
+		else:
+			file_name = "{}/{}.mp3".format(album, title)
 		u = urlopen(download_url)
-		f = open(file_name, 'wb')
+		try:
+			f = open(file_name, 'wb')
+		except Exception as e:
+			print("file open Exception")
+			f = open(file_name, 'wb')
 		meta = u.info()
 		file_size = int(meta["Content-Length"])
 		print("Downloading: {} (Bytes: {})".format(title, file_size))
@@ -38,35 +121,20 @@ def main(url):
 		meta['title'] = title
 		meta['album'] = album
 		meta['artist'] = artist
+		meta['tracknumber'] = track
 		meta.save()
 
 	print("Hold on, getting the relevant info...")
-	sock = urlretrieve(url)
-	ch = open(sock[0]).read()
-	RE = re.compile('album_title : "(.*?)",')
-	album = RE.findall(ch)
-	RE = re.compile('artist : "(.*?)",')
-	artist = RE.findall(ch)
-	RE = re.compile("trackinfo : \[(.*?)\]", re.M)
-	matches = RE.findall(ch)
-	try:
-		RE = re.compile('"title":"(.*?),"file')
-		titles = RE.findall(matches[0])
-		RE = re.compile('"file":{"mp3-128":"(.*?)"}')
-		songs = RE.findall(matches[0])
-	except Exception as e:
-		print("I can't download individual tracks right now, only albums. This might get fixed in the futured :)")
-		print("You're welcome to try again, but this time with an actual album")
-		sys.exit()
-
-	directory = os.path.join(album[0])
-	if not os.path.exists(directory):
-		os.makedirs(directory)
-	for i in range(len(songs)):
-		title = titles[i].replace('"','')
-		download(songs[i], title, album[0], artist[0])
+	if "bandcamp" in reply.split("."):
+		simple_parse(reply)
+	else:
+		parse_username(reply)
+	
 	
 	
 if __name__ == '__main__':
-	url = input("Enter the url of album you wish to download: ")
-	main(url)
+	print("You have two options")
+	print("[1] Enter the full url to the album or track you wish to download")
+	print('[2] Enter the username of the artist, and I will try to download everything the artist has made available, ("username".bandcamp.com)')
+	reply = input("You must choose, but choose wisely: ")
+	main(reply)
